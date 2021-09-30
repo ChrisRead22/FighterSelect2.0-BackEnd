346742aae5df85a684e12cc51808a1160450e788
Christopher Read <Christopherread22@outlook.com>
2021-09-30 18:43:14 -0400

    more fighters

--- db/seeds.rb
+++ db/seeds.rb
@@ -79,7 +79,13 @@ FighterTB6 = Fighter.create(
     supermove: "During Rage, 1+2"
 )
 
-
+FighterTB7 = Fighter.create(
+    fighting_game_id: FightingGame1.id,
+    fight_style_id: FightStyle1.id,
+    name: "Bob",
+    image: "https://64.media.tumblr.com/58efba9c2b7ce30ed123a3acd8dec81d/tumblr_p4qi8eeKCc1w37z0to3_640.gifv",
+    supermove: "During Rage, Down/Right + 1 + 1"
+)
 
 FighterTG1 = Fighter.create(
     fighting_game_id: FightingGame1.id,
